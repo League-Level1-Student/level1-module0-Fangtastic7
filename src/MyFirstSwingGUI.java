758be7d2ba471e7810aa4c24bfffe492e095320d
Fangtastic7 <league@WTS20.attlocal.net>
2018-03-31 11:33:02 -0700

    MyFirstSwingGUI, PhotQuiz

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,32 +15,33 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-		JFrame object = null;
+		JFrame object = new JFrame();
 		//2. Set your JFrame object to be visible
 		object.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 			
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel item1 = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel item2 = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+		item2.setText("Good Morning!");
 		//8. Add the JPanel object to the JFrame
-
+			object.add(item1);
 		//9. Add the JLabel object to the JPanel
-
+			item1.add(item2);
 		//10. Pack your JFrame.
-
+			object.pack();
 		//11. Run your program again. Do you see your message.
-		
+		//no
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+			loadImage();
 		//13. Re-pack the JFrame object.
-		
+		object.pack();
 		//14. Run the program one more time. Do you see the image?
+		//yes
 	}
 	
 	public static ImageIcon loadImage(){
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -6,6 +6,9 @@
 
 import java.awt.Component;
 import java.awt.Frame;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,54 +18,99 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class PhotoQuiz {
-
+public class PhotoQuiz 
+implements MouseMotionListener
+{
+	
 	public static void main(String[] args) throws Exception {
 		Frame quizWindow = new Frame();
 		quizWindow.setVisible(true);
-
+			int score = 0;
+			
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image URL”)
-
+			String doggo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf-8InGizc0Sddp460QQ7gisQTeGD867UGa5DaaWoMkk67hEYN";
 		// 2. create a variable of type "Component" that will hold your image
-
+			
+			
+			Component image1;
 		// 3. use the "createImage()" method below to initialize your Component
-
+			image1 = createImage(doggo);
+			AtMouseComponent(image1);
 		// 4. add the image to the quiz window
-
+			quizWindow.add(image1);
 		// 5. call the pack() method on the quiz window
-
+			quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+			String question = JOptionPane.showInputDialog("What type of dog is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+			if(question.equalsIgnoreCase("doge")) {
+				score = score +1;
+				JOptionPane.showMessageDialog(null, "CORRECT! Score: " + score );
+				
+				
+			}
 		// 8. print "INCORRECT" if the answer is wrong
-
+			else {
+				JOptionPane.showMessageDialog(null, "INCORRECT Score: " + score);
+			}
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+			quizWindow.remove(image1);
 		// 10. find another image and create it (might take more than one line of code)
-
+			String kermit = "https://i.pinimg.com/originals/ba/e8/23/bae82312ee7e96e22987459541037281.png";
+			Component image2; 
+			image2 = createImage(kermit);
+			AtMouseComponent(image2);
 		// 11. add the second image to the quiz window
-
+			quizWindow.add(image2);
 		// 12. pack the quiz window
-
+			quizWindow.pack();
 		// 13. ask another question
-
-		// 14+ check answer, say if correct or incorrect, etc.
-
+			String question2 = JOptionPane.showInputDialog("What type of animal is this?")
+;		// 14+ check answer, say if correct or incorrect, etc.
+			if(question2.equalsIgnoreCase("frog")) {
+				score = score +1;
+				JOptionPane.showMessageDialog(null, "CORRECT! Score: " + score);
+			}
+			else {
+				
+				JOptionPane.showMessageDialog(null, "INCORRECT Score: " + score);
+			}
+			JOptionPane.showMessageDialog(null, "Total Score: " + score );
+			
+				
 	}
-
+	
 	private static Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
 		JLabel imageLabel = new JLabel(icon);
+		
+		
+			
 		return imageLabel;
 	}
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
+	
 	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
-}
+	 public static void AtMouseComponent(Component imageComponent) {
+		 System.out.println("Hello!");
+	 }
 
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 
 
 
